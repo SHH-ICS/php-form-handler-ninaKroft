75572235cb99678fc26e24802dfd28f60b2ab493
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-02-27 18:25:42 +0000

    work

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-CA">
   <head>
     <title>First php web page</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Nina Kroft">
     <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png" />
